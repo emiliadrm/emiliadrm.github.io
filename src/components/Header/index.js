69a7a8e525a822b3a04642970d97ffdd9222a9dd
William Bettencourt <aries.98.mu@gmail.com>
2018-08-30 22:25:41 -0300

    minor update

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.scss';
+
+const Header = () => {
+  return (
+    <header className="hero-header">
+      <h1>Emilia Rondón</h1>
+      <h3>Designer</h3>
+    </header>
+  )
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import './index.scss';
 
-const App = () => {
-  return (
-    <div>
-      <h1>Emi TQM</h1>
-    </div>
-  );
+class App extends React.Component {
+  componentDidCatch(error, info) {
+    console.error('Error in app', error, info);
+  }
+  render() {
+    return (
+      <div className="main">
+        <Header />
+        <Home />
+      </div>
+    );
+  }
 }
 
 render(<App />, document.getElementById("main"));
