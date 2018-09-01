202e24b8eef527c29ed761d6f20108e0afd9ca98
William Bettencourt <aries.98.mu@gmail.com>
2018-08-31 22:00:33 -0300

    adding aphrodite

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import './header.scss';
+import { StyleSheet, css } from 'aphrodite';
 
 const Header = () => {
   return (
-    <header className="hero-header">
+    <header className={css(styles.heroHeader)}>
       <h1>Emilia Rondón</h1>
       <h3>Designer</h3>
     </header>
   )
 }
+
+const styles = StyleSheet.create({
+  heroHeader: {
+    padding: '8rem 4rem',
+    textAlign: 'center',
+    color: '#FFF',
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+  },
+});
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { StyleSheet, css } from 'aphrodite';
+
 import Header from './components/Header';
 import Home from './components/Home';
+
 import './index.scss';
 
 class App extends React.Component {
@@ -10,12 +13,19 @@ class App extends React.Component {
   }
   render() {
     return (
-      <div className="main">
+      <div className={css(styles.main)}>
         <Header />
         <Home />
       </div>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  main: {
+    width: '80%',
+    margin: '0 auto',
+  }
+});
 
 render(<App />, document.getElementById("main"));
